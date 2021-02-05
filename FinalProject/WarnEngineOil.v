8c6bb91628edc8a42a8a2f8f3dd72ebb5b16540b
Mohammad Mahdi Abdollahpour <mohammadmahdiabdollahpour@gmail.com>
2021-02-05 12:51:14 +0330

    Initial testbench/wave output

--- FinalProject/WarnEngineOil.v
+++ FinalProject/WarnEngineOil.v
@@ -1,35 +1,34 @@
-module WarnGas ();
+module WarnEngineOil1 (clock, reset, engine_oil, warn_engine_oil);
 
-input   clock, reset, integer engine_oil;
+input clock;
+input reset;
+input wire signed [31:0] engine_oil;
 output reg warn_engine_oil;
 
 parameter SIZE  = 2;
 reg   [SIZE-1:0] state;
 
 always @ (posedge clock)
-begin: 
+begin
 if (reset == 1) 
-    begin
-        state <= #1 8'd0;
-    end 
+  state <= #1 8'd0;
 else
  case(state)
-    8'd0 : begin
+    2'd0 : begin
               if (engine_oil < 10)
-                state <= 8'd1;
+                state <= 2'd1;
               else 
-                state <= 8'd2;
+                state <= 2'd2;
            end
-    8'd1 : begin
+    2'd1 : begin
             warn_engine_oil <= 1;
-            state <= 8'd0;
+            state <= 2'd0;
            end
-
-    8'd2 : begin
+    2'd2 : begin
             warn_engine_oil <= 0;
-            state <= 8'd0;
+            state <= 2'd0;
            end
-   default : state <= 8'd0;
+   default : state <= 2'd0;
 endcase
 end
 
---
+++ FinalProject/WarnEngineOilTB.v
@@ -0,0 +1,40 @@
+// `timescale 1ns / 1ps
+
+module WarnEngineOilTB ();
+
+reg clock, reset;
+reg signed [31:0] engine_oil;
+wire warn_engine_oil;
+
+WarnEngineOil1 inst(
+  clock, 
+  reset, 
+  engine_oil, 
+  warn_engine_oil);
+
+initial begin
+clock = 0;
+forever #10 clock = ~clock;
+end 
+
+initial begin
+// Initialize Inputs
+  $dumpfile("test.vcd");
+  $dumpvars(0, WarnEngineOilTB);
+  engine_oil = 0;
+  reset = 1;
+  // Wait 100 ns for global reset to finish
+  #30;
+      reset = 0;
+  #40;
+  engine_oil = 10;
+  #10; // wait for period 
+  engine_oil = 9;
+  #10;
+  engine_oil = 10; 
+  #20;
+  engine_oil = 30; 
+  #500 $finish;
+ end
+      
+endmodule
